bcdb0b7a90414024127c1b931d05f5ca04cce3c4
user786-debug <dufferyouidiot@.com>
2026-01-31 10:11:14 +0500

    added verifying log in app.js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config(); // Load environment variables
-require('./config/db'); // Initialize DB connection
-const path = require('path');
-const { version } = require('os');
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const SupportManagersRoutes = require('./routes/supportManagersRoutes');
-const starsManagersRoutes = require('./routes/starManagersRoutes');
-const policyDocumentsRoutes = require('./routes/policyDocumentsRoutes');
-const versionControlRoutes = require('./routes/versionControlRoutes');
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config(); // Load environment variables
+require("./config/db"); // Initialize DB connection
+const path = require("path");
+const { version } = require("os");
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const SupportManagersRoutes = require("./routes/supportManagersRoutes");
+const starsManagersRoutes = require("./routes/starManagersRoutes");
+const policyDocumentsRoutes = require("./routes/policyDocumentsRoutes");
+const versionControlRoutes = require("./routes/versionControlRoutes");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -19,17 +19,23 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.json());
 
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-
+app.get("/", (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Admin Panel API is running 🚀",
+    timestamp: new Date().toISOString(),
+  });
+});
 
 // Routes
-app.use('/api/admin', authRoutes);
-app.use('/api/user', userRoutes);
-app.use('/api/SupportManager', SupportManagersRoutes);
-app.use('/api/starManager', starsManagersRoutes);
-app.use('/api/policyDocument', policyDocumentsRoutes);
-app.use('/api/version', versionControlRoutes);
+app.use("/api/admin", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/SupportManager", SupportManagersRoutes);
+app.use("/api/starManager", starsManagersRoutes);
+app.use("/api/policyDocument", policyDocumentsRoutes);
+app.use("/api/version", versionControlRoutes);
 
 // Start server
 app.listen(PORT, () => {
